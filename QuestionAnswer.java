2e22a8776f0b7bda1ecd2adb5c085f0a7d5993cd
VaishSabbanwar <168840863+VaishSabbanwar@users.noreply.github.com>
2024-09-10 23:47:30 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,28 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+
+    public static String question[]={
+      "Grand Central Terminal, Park Avenue, New York is the world's ?",
+      "Entomology is the science that studies ?",
+      "Garampani sanctuary is located at ?",
+      "For which of the following disciplines is Nobel Prize awarded?",
+      "ICAO stands for-"
+    };
+
+    public static String choices[][]={
+            {"largest railway station", "highest railway station", "longest railway station","None of the above"},
+            {"Behavior of human beings", "Insects", "The origin and history of technical and scientific terms","The formation of rocks"},
+            {"Junagarh, Gujarat" ,"Diphu, Assam" ,"Kohima, Nagaland" ,"Gangtok, Sikkim"},
+            {"Physics and Chemistry", "Physiology or Medicine", "Literature, Peace and Economics", "All of the above"},
+            {"International Civil Aviation Organization", "Indian Corporation of Agriculture Organization", "Institute of Company of Accounts Organization", "None of the above"}
+    };
+
+    public static String correctAnswers[]={
+      "largest railway station",
+      "Insects",
+      "Diphu, Assam",
+      "All of the above",
+      "International Civil Aviation Organization"
+    };
+}
